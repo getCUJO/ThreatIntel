437e82bb00f6bff63c0b9aa569530489fec3bad2
padorka <66425539+padorka@users.noreply.github.com>
2021-10-07 11:27:45 +0200

    Update go_func.py
    Updated to work with Go version 1.16.
    Updated to work with 64 bit big endian binaries.

--- Scripts/Ghidra/go_func.py
+++ Scripts/Ghidra/go_func.py
@@ -18,19 +18,36 @@ def getGopclntab():
     print "No .gopclntab section found."
     return None
 
-def renameFunc(start):
-    ptr_size_address = start.add(7)
-    ptr_size = getByte(ptr_size_address)
-    section_size_address = start.add(8)
-    section_size = getInt(section_size_address)
-    p = start.add(8 + ptr_size)
-    for i in range (section_size):
-        func_offset = getInt(p)
-        func_address = currentProgram.getAddressFactory().getAddress(hex(func_offset))
-        p = p.add(ptr_size)
-        name_offset = getInt(p)
-        p = p.add(ptr_size)
-        name_pointer = start.add(name_offset + ptr_size)
+#Check if Go version is 1.16 or above
+def isVer116():
+    magic116 = {'0xfffffffa', '0xfaffffff'}
+    createDWord(start)
+    if  getDataAt(start).getValue().toString() in magic116:
+        print "ver116"
+        return True
+    return False
+
+#Recover function names for Go versions 1.2 - 1.15
+def renameFunc12(start):
+    ptrsize= getByte(start.add(7))
+    if ptrsize == 8:
+        nfunctab = getLong(start.add(8))
+    else:
+        nfunctab = getInt(start.add(8))
+    functab = start.add(8 + ptrsize)
+
+    p = functab
+    for i in range (nfunctab):
+        if ptrsize == 8:
+            func_address = currentProgram.getAddressFactory().getAddress(hex(getLong(p)).rstrip("L"))
+            p = p.add(ptrsize)
+            name_offset = getLong(p)
+        else:
+            func_address = currentProgram.getAddressFactory().getAddress(hex(getInt(p)))
+            p = p.add(ptrsize)
+            name_offset = getInt(p)
+        p = p.add(ptrsize)
+        name_pointer = start.add(name_offset + ptrsize)
         name_address = start.add(getInt(name_pointer))
         func_name = getDataAt(name_address)
 
@@ -42,7 +59,6 @@ def renameFunc(start):
                 print "ERROR: No name" 
                 continue
 
-        #print "address:0x%x, data:%s" %(func_offset, func_name.getValue())
         
         func = getFunctionAt(func_address)
         if func is not None:
@@ -53,6 +69,58 @@ def renameFunc(start):
             func = createFunction(func_address, func_name.getValue())
             print "New function created: %s" % func_name
 
+#Recover function names for Go versions 1.16 and above
+def renameFunc116(start):
+    ptrsize= getByte(start.add(7))
+    if ptrsize == 8:
+        nfunctab = getLong(start.add(8))
+        offset = getLong(start.add(8 + 2*ptrsize))
+        funcnametab = start.add(offset)
+        offset = getLong(start.add(8 + 6*ptrsize))
+    else:
+        nfunctab = getInt(start.add(8))
+        offset = getInt(start.add(8 + 2*ptrsize))
+        funcnametab = start.add(offset)
+        offset = getInt(start.add(8 + 6*ptrsize))
+    functab = start.add(offset)
+
+    p = functab
+    for i in range (nfunctab):
+        if ptrsize == 8:
+            func_address = currentProgram.getAddressFactory().getAddress(hex(getLong(p)).rstrip("L"))
+            p = p.add(ptrsize)
+            funcdata_offset = getLong(p)
+        else:
+            func_address = currentProgram.getAddressFactory().getAddress(hex(getInt(p)))
+            p = p.add(ptrsize)
+            funcdata_offset = getInt(p)
+        p = p.add(ptrsize)
+        name_pointer = functab.add(funcdata_offset + ptrsize)
+        name_address = funcnametab.add(getInt(name_pointer))
+        func_name = getDataAt(name_address)
+
+        #Try to define function name string.
+        if func_name is None:
+            try:
+                func_name = createAsciiString(name_address)
+            except:
+                print "ERROR: No name" 
+                continue
+
+        
+        func = getFunctionAt(func_address)
+        if func is not None:
+            func_name_old = func.getName()
+            func.setName(func_name.getValue().replace(" ", ""), USER_DEFINED)
+            print "Function %s renamed as %s" % (func_name_old, func_name.getValue())
+        else:
+            func = createFunction(func_address, func_name.getValue())
+            print "New function created: %s" % func_name
+
+
 start = getGopclntab()    
 if start is not None:
-    renameFunc(start)
+    if isVer116():
+        renameFunc116(start)
+    else:
+        renameFunc12(start)
